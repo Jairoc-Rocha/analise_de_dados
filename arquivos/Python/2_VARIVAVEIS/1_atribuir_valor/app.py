60bb2d4840a7d608f7d559f008e99204809e59e6
Jairo Rocha <jairoc.rocha10@gmail.com>
2024-01-19 15:08:25 -0300

    Aprendendo Python

---
+++ arquivos/Python/2_VARIVAVEIS/1_atribuir_valor/app.py
@@ -0,0 +1,9 @@
+nome = "Matheus"
+
+print(nome)
+print(type(nome))
+
+idade = 29
+
+print(idade)
+print(type(idade))
---
+++ arquivos/Python/2_VARIVAVEIS/3_nomes_validos/app.py
@@ -0,0 +1,15 @@
+ano2020 = 2020
+
+print(ano2020)
+
+_2021ano = 2021
+
+print(_2021ano)
+
+testando_espaco = "teste"
+
+print(testando_espaco)
+
+nome_completo = "Matheus Battisti"
+
+print(nome_completo)
