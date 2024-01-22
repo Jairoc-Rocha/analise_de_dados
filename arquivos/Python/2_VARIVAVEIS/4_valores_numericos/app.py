0ce9f5e5c430d36972839389b121e0f353b088f1
Jairo Rocha <jairoc.rocha10@gmail.com>
2024-01-21 21:10:46 -0300

    outro commit

--- arquivos/Python/2_VARIVAVEIS/4_valores_numericos/app.py
+++ arquivos/Python/2_VARIVAVEIS/4_valores_numericos/app.py
@@ -1,11 +1,9 @@
 numero = 100
 
 print(numero)
-
 print(type(numero))
 
 n = 12.59
 
 print(n)
-
 print(type(n))
--- arquivos/Python/2_VARIVAVEIS/5_exercicio_4/app.py
+++ arquivos/Python/2_VARIVAVEIS/5_exercicio_4/app.py
@@ -1,6 +1,6 @@
-x = 100
-y = 50.50
+n1 = 100
+n2 = 50.50
+n3 = n1 + n2
 
-z = x + y
-
-print(z)
+print(n3)
+print(type(n3))
